480720d04c71da7591e6c5e7402a482c297106df
franckapik <fanch44@hotmail.com>
2020-04-20 14:08:55 +0200

    Mise en place de react

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
@@ -18,9 +17,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views/build')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/views/build/index.html`));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
