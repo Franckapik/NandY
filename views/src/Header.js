fc37cb6c3a65ca508f60d6e4f3b544049051fd44
franckapik <fanch44@hotmail.com>
2020-04-20 16:13:45 +0200

    Ajout Three.js (essai rpi)

---
+++ views/src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles/App.scss';
+
+function Header() {
+  return (
+    <ul className="header flex_r">
+    <li>Logo miniature</li>
+    <li>Agenda</li>
+    <li>Galeries</li>
+    <li>Echanges</li>
+    <li>contact@ń&y.fr</li>
+  </ul>);
+}
+
+export default Header;
---
+++ views/src/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles/App.scss';
+
+function Main() {
+  return (<div className="main">
+Que passa
+<p>por la calle</p>
+  </div>);
+}
+
+export default Main;
